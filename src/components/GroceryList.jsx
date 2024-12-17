b657f80b5c93d4fc7b7023f7ffd658a481d314f0
Levi Duarte <lduart441@west-mec.org>
2024-12-17 16:50:30 -0700

    new imgs added

--- src/components/GroceryList.jsx
+++ src/components/GroceryList.jsx
@@ -3,15 +3,18 @@ import Star from "./Star";
 export default function GroceryList({ items }) {
   return (
     <div className={styles.list}>
-      {items.map(i => (
-        <div key={i.id} className={styles.item}>
-          <h2>{i.item}</h2>
-          <p>Category: {i.category}</p>
-          <p>DETAILS: {i.details}</p>
-          <p>Price: ${i.price_in_usd}</p>
-          <p>IMG: {i.product_img}</p>
-        </div>
-      ))}
+      {items.map((i) => {
+        const image = new URL(i.product_img, import.meta.url).href;
+        return (
+          <div key={i.id} className={styles.item}>
+            <h2>{i.item}</h2>
+            <p>Category: {i.category}</p>
+            <p>DETAILS: {i.details}</p>
+            <p>Price: ${i.price_in_usd}</p>
+            <img src={image} alt="new img" />
+          </div>
+        );
+      })}
     </div>
   );
 }
